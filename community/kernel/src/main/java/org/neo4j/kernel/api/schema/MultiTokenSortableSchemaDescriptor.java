e90138dce250077f520972de3baf05b95af164d5
Alex Iudice <alex.iudice@graphfoundation.com>
2020-02-07 15:26:41 -0500

    Experimental changes to 'MultiTokenSortableSchemaDescriptor'.

--- community/kernel/src/main/java/org/neo4j/kernel/api/schema/MultiTokenSortableSchemaDescriptor.java
+++ community/kernel/src/main/java/org/neo4j/kernel/api/schema/MultiTokenSortableSchemaDescriptor.java
@@ -1,5 +1,8 @@
 package org.neo4j.kernel.api.schema;
 
+import org.apache.commons.lang3.ArrayUtils;
+import org.neo4j.internal.kernel.api.schema.SchemaComputer;
+import org.neo4j.internal.kernel.api.schema.SchemaProcessor;
 import org.neo4j.storageengine.api.EntityType;
 
 public class MultiTokenSortableSchemaDescriptor extends MultiTokenSchemaDescriptor
@@ -20,5 +23,40 @@ public class MultiTokenSortableSchemaDescriptor extends MultiTokenSchemaDescript
     public int[] getSortIds()
     {
         return sortIds;
+    }
+
+    // I think this is used to determine which properties to pick up for indexing! 
+    @Override
+    public int[] getPropertyIds()
+    {
+        int[] propertyIds = super.getPropertyIds();
+        return ArrayUtils.addAll(propertyIds, sortIds);
+    }
+
+    @Override
+    public boolean isAffected( long[] entityTokenIds )
+    {
+        boolean affected = super.isAffected( entityTokenIds );
+        return affected;
+//        for ( int id : sortIds )
+//        {
+//            if ( ArrayUtils.contains( entityTokenIds, id ) )
+//            {
+//                return true;
+//            }
+//        }
+//        return false;
+    }
+
+    @Override
+    public <R> R computeWith( SchemaComputer<R> computer )
+    {
+        return computer.computeSpecific( this );
+    }
+
+    @Override
+    public void processWith( SchemaProcessor processor )
+    {
+        processor.processSpecific( this );
     }
 }
