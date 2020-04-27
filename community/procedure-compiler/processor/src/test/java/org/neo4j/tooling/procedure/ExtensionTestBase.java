a297ee3722a18a5808372807325ee50eb0cd92e7
Brad Nussbaum <brad@graphfoundation.org>
2020-04-27 18:00:28 -0400

    Fixing tests that fail from number of lines

--- community/procedure-compiler/processor/src/test/java/org/neo4j/tooling/procedure/ExtensionTestBase.java
+++ community/procedure-compiler/processor/src/test/java/org/neo4j/tooling/procedure/ExtensionTestBase.java
@@ -56,20 +56,20 @@ public abstract class ExtensionTestBase
         unsuccessfulCompilationClause.withErrorContaining(
                 "@org.neo4j.procedure.Context usage error: field BadContextFields#shouldBeNonStatic should be public, " +
                 "non-static and non-final" )
-                .in( sproc ).onLine( 35 );
+                .in( sproc ).onLine( 38 );
 
         unsuccessfulCompilationClause.withErrorContaining(
                 "@org.neo4j.procedure.Context usage error: field BadContextFields#shouldBeNonFinal should be public, " +
                 "non-static and non-final" )
-                .in( sproc ).onLine( 38 );
+                .in( sproc ).onLine( 41 );
 
         unsuccessfulCompilationClause.withErrorContaining(
                 "@org.neo4j.procedure.Context usage error: field BadContextFields#shouldBePublic should be public, " +
                 "non-static and non-final" )
-                .in( sproc ).onLine( 42 );
+                .in( sproc ).onLine( 45 );
 
         unsuccessfulCompilationClause.withErrorContaining( "Field BadContextFields#shouldBeStatic should be static" )
-                .in( sproc ).onLine( 43 );
+                .in( sproc ).onLine( 46 );
     }
 
     @Test
@@ -84,7 +84,7 @@ public abstract class ExtensionTestBase
                 ".GraphDatabaseAPI> on BadContextRestrictedTypeField#notOfficiallySupported.\n" +
                 "  The procedure will not load unless declared via the configuration option 'dbms.security.procedures.unrestricted'.\n" +
                 "  You can ignore this warning by passing the option -AIgnoreContextWarnings to the Java compiler" )
-                .in( sproc ).onLine( 35 );
+                .in( sproc ).onLine( 38 );
     }
 
     @Test
@@ -112,6 +112,6 @@ public abstract class ExtensionTestBase
                                 TerminationGuard.class.getName() + ">, <" +
                                 SecurityContext.class.getName() + ">, <" +
                                 ProcedureTransaction.class.getName() + ">" )
-                .in( sproc ).onLine( 33 );
+                .in( sproc ).onLine( 36 );
     }
 }
--- community/procedure-compiler/processor/src/test/java/org/neo4j/tooling/procedure/UserFunctionProcessorTest.java
+++ community/procedure-compiler/processor/src/test/java/org/neo4j/tooling/procedure/UserFunctionProcessorTest.java
@@ -56,10 +56,10 @@ public class UserFunctionProcessorTest extends ExtensionTestBase
                         .withErrorCount( 2 );
 
         compilation.withErrorContaining( "@org.neo4j.procedure.Name usage error: missing on parameter <parameter>" )
-                .in( function ).onLine( 28 );
+                .in( function ).onLine( 31 );
 
         compilation.withErrorContaining( "@org.neo4j.procedure.Name usage error: missing on parameter <otherParam>" )
-                .in( function ).onLine( 28 );
+                .in( function ).onLine( 31 );
     }
 
     @Test
@@ -72,7 +72,7 @@ public class UserFunctionProcessorTest extends ExtensionTestBase
                 .withErrorContaining(
                         "Unsupported return type <java.util.stream.Stream<java.lang.Long>> of function defined in " +
                 "<org.neo4j.tooling.procedure.procedures.invalid.bad_return_type.BadReturnTypeUserFunction#wrongReturnTypeFunction>" )
-                .in( function ).onLine( 36 );
+                .in( function ).onLine( 39 );
     }
 
     @Test
@@ -84,7 +84,7 @@ public class UserFunctionProcessorTest extends ExtensionTestBase
         assert_().about( javaSource() ).that( function ).processedWith( processor() ).failsToCompile().withErrorCount( 1 )
                 .withErrorContaining(
                         "Unsupported parameter type <short> of procedure|function BadPrimitiveInputUserFunction#doSomething" )
-                .in( function ).onLine( 32 );
+                .in( function ).onLine( 35 );
     }
 
     @Test
@@ -99,15 +99,15 @@ public class UserFunctionProcessorTest extends ExtensionTestBase
 
         compilation.withErrorContaining( "Unsupported parameter type " +
                 "<java.util.List<java.util.List<java.util.Map<java.lang.String,java.lang.Thread>>>>" +
-                " of procedure|function BadGenericInputUserFunction#doSomething" ).in( function ).onLine( 36 );
+                " of procedure|function BadGenericInputUserFunction#doSomething" ).in( function ).onLine( 39 );
 
         compilation.withErrorContaining( "Unsupported parameter type " +
                 "<java.util.Map<java.lang.String,java.util.List<java.util.concurrent.ExecutorService>>>" +
-                " of procedure|function BadGenericInputUserFunction#doSomething2" ).in( function ).onLine( 42 );
+                " of procedure|function BadGenericInputUserFunction#doSomething2" ).in( function ).onLine( 45 );
 
         compilation.withErrorContaining(
                 "Unsupported parameter type <java.util.Map> of procedure|function BadGenericInputUserFunction#doSomething3" )
-                .in( function ).onLine( 48 );
+                .in( function ).onLine( 51 );
     }
 
     @Test
